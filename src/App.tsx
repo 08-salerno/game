c7ee545f7d059ce9148ebc4c89c351c37a6dc966
Vladimir Sitnik <vovasitnik@gmail.com>
2021-09-19 01:49:24 +0300

    feat!: входная точка в приложение с настроенным роутингом
    В вебпак и дев-сервер добавлены настройки, чтобы рефреш страницы редиректил на index

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  RouteProps
+} from 'react-router-dom';
+
+type AppRoute = {
+  title: string;
+  link: string;
+} & RouteProps &
+  Required<Pick<RouteProps, 'component'>>;
+
+/**
+ * Добавляй руты сюда, а не в разметку
+ */
+const routes: AppRoute[] = [];
+
+const App: React.FC = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Главная</Link>
+          </li>
+          {routes.map((route: AppRoute) => (
+            <li>
+              <Link to={route.link}>{route.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <Switch>
+        {routes.map((route: AppRoute) => (
+          /**
+           * Добавь недостающий пропс
+           */
+          <Route path={route.path ? route.path : route.link}>{route.component}</Route>
+        ))}
+        <Route path="/">
+          <h1>Отсюда всё начинается :)</h1>
+          Возможно логично сделать стартовую страницу сразу с игрой и перенаправлять на
+          страницу с игрой при странных рутах
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/static/index.html',
-            title: 'Development'
-        }),
-        new ForkTsCheckerWebpackPlugin({
-            eslint: {
-                files: "./src/**/*.tsx",
-            },
-        }),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        static: ['dist'],
-        port: 3000,
-        client: {
-            overlay: true
-        },
-        headers: {
-            'Access-Control-Allow-Origin': '*'
+  mode: 'development',
+  entry: './src/index.tsx',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/static/index.html',
+      title: 'Development'
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.tsx'
+      }
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: ['dist'],
+    port: 3000,
+    client: {
+      overlay: true
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ]
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react',
-                        '@babel/preset-typescript'
-                    ]
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  }
 };
