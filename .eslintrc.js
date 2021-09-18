6f810086a847f9f6997a933a963ee10e9d5a76fb
vargin <vargin@mkb.ru>
2021-09-18 23:35:58 +0300

    Added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,113 @@
 module.exports = {
-    extends: ['airbnb', 'prettier']
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+    node: true
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.eslint.json"
+  },
+  rules: {
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/destructuring-assignment': 'warn',
+    'react/no-array-index-key': 'off',
+    'jsx-a11y/no-autofocus': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/control-has-associated-label': 'warn',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'import/prefer-default-export': 'warn',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'warn',
+    'import/no-cycle': 'off',
+    'dot-notation': 'warn',
+    'consistent-return': 'off',
+    'react/jsx-indent': 'off',
+    'no-unused-vars': 'warn',
+    'spaced-comment': 'off',
+    'no-alert': 'off',
+    'no-restricted-globals': 'warn',
+    quotes: [
+      'warn',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'off',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-use-before-define': 'off',
+    'no-plusplus': 'off',
+    'no-shadow': 'off',
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+        tabWidth: 2,
+        comments: 120,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    "react/prop-types": "off",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    //TODO
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    //TODO
+    "@typescript-eslint/no-shadow": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-floating-promises": "warn",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true }
+    ]
+  }
+};
+
+
