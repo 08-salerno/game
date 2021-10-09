08df2d035d0ca620109aef552355489a71984189
vargin <vargin@mkb.ru>
2021-10-09 09:12:16 +0300

    start setup for tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Normalize } from 'styled-normalize';
 import App from './App';
@@ -11,3 +11,43 @@ const Root: React.VFC = () => (
 );
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
+
+
+
+sonarCube
+параметры
+agile scrum
+waterfall
+
+по трем точкам
+композиция сверху вниз
+снизу вверх
+
+export default function Countdown() {
+  const [num, setNum] = useState(100);
+  const [pause, setPause] = useState(false);
+
+  const handleClick = () => {
+    setPause(!pause);
+  };
+
+  useEffect(() => {
+    let i;
+    if (pause) {
+      i = setInterval(() => {
+        setNum((prev) => prev + 1);
+      });
+    }
+
+    return () => {
+      clearInterval(i);
+    };
+  }, [pause]);
+
+  return (
+    <div>
+      <div>{num}</div>
+      <button onClick={handleClick}>{pause ? 'Run' : 'Pause'}</button>
+    </div>
+  );
+}
