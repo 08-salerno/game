a339bff1a6ec9b93a6263820bac0dde1918e08b4
Dmitry Vargin <davrgin@mail.com>
2021-11-24 00:35:38 +0300

    added app docker config to compose and unsuccessfully tried connect to DB

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const { Client } = require('pg');
+const { Sequelize } = require('sequelize-typescript');
 
 const PORT = process.env.PORT || 3001;
 
 const STATIC = path.resolve(__dirname, 'dist');
 const INDEX = path.resolve(STATIC, 'index.html');
+
+// const sequelizeOptions = {
+//   host: 'postgres',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   database: 'postgres',
+//   dialect: 'postgres', // 'mysql', 'sqlite', 'mariadb', 'mssql'
+// };
+//
+// // eslint-disable-next-line no-unused-vars
+// const sequelize = new Sequelize(sequelizeOptions);
+
+const client = new Client({
+  user: 'postgres',
+  host: 'postgres',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+});
+
+client.connect();
+
+console.log(66666);
+
+client.query('SELECT NOW()').then((res) => {
+  console.log(5555555, res.rows);
+  client.end();
+})
 
 const app = express();
 app.use(cors({
