2cbb87d94faa12cd01c89b491452d2a9b91678ca
Vladimir Sitnik <vovasitnik@gmail.com>
2021-12-06 20:38:58 +0300

    improvement(docker): актуализация Dockerfile под сборку ssr и нового сервера

--- server/database/db-client.ts
+++ server/database/db-client.ts
@@ -1,14 +1,15 @@
 import { Sequelize } from 'sequelize';
 
+// todo [sitnik] вынести в переменные окружения
 // При запуске из docker обязательно host: postgres; port: 5432
 // При запуске с хостовой машины host: localhost; port: 5555
 const client = new Sequelize({
   dialect: 'postgres',
   username: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   database: 'postgres',
   password: 'postgres',
-  port: 5555,
+  port: 5432,
 });
 
 export default client;
